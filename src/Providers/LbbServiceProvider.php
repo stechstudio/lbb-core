e7dcadaf27bbbc9461435c2281643937f1c5c613
Bubba <rob@stechstudio.com>
2019-10-04 16:58:35 -0400

    A service provider to button it all up.

---
+++ src/Providers/LbbServiceProvider.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace STS\LBB\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use STS\LBB\Lambda\Application as LambdaApplication;
+use STS\LBB\Lambda\Contracts\Application as LambdaApplicationContract;
+use STS\LBB\Lambda\Contracts\Registrar;
+use STS\LBB\Lambda\Router;
+
+
+class LbbServiceProvider extends ServiceProvider
+{
+    /**
+     * Default path to laravel configuration file in the package
+     *
+     * @var string
+     */
+    protected $configPath = __DIR__.'/../../config/lbb.php';
+
+    /**
+     * Default path to publish the lambda routes file from.
+     *
+     * @var string
+     */
+    protected $routesPath = __DIR__.'/../../routes/lambda.php';
+
+    public function register(): void
+    {
+        $this->app->singleton(
+            LambdaApplicationContract::class,
+            LambdaApplication::class
+        );
+
+        $this->app->alias(
+            LambdaApplicationContract::class,
+            'lbb.contract.application'
+        );
+        $this->app->alias(
+            LambdaApplicationContract::class,
+            'lbb.application'
+        );
+        
+        $this->app->singleton(Registrar::class, Router::class);
+        $this->app->alias(
+            Registrar::class,
+            'lbb.router'
+        );
+    }
+}
