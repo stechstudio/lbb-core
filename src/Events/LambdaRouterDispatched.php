d3a2e7bb5d2f6b54d49cc7e36dbe98cfd7f3e7d7
Bubba <rob@stechstudio.com>
2019-10-04 16:53:51 -0400

    Some router dispatch events

---
+++ src/Events/LambdaRouterDispatched.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+/**
+ * Package: laravel-bref-bridge
+ * Create Date: 2019-03-02
+ * Created Time: 10:03
+ */
+
+namespace STS\LBB\Events;
+
+use STS\LBB\Lambda\Contracts\Registrar;
+
+class LambdaRouterDispatched
+{
+    /** @var Registrar */
+    public $router;
+
+    public function __construct(Registrar $router)
+    {
+        $this->router = $router;
+    }
+}
