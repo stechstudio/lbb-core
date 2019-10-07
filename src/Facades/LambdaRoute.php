7f66fea3ff24d38fd04d8a3e4ba0821ee21d579c
Bubba <rob@stechstudio.com>
2019-10-07 12:21:39 -0400

    Setup a facade for lambda routes

---
+++ src/Facades/LambdaRoute.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace STS\LBB\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static array dispatch(Event $event, Context $context)
+ * @method static bool hasController(string $eventName)
+ * @method static Registrar forget(string $eventName)
+ * @method static Registrar register(string $eventName, callable $controller)
+ * @method static Registrar registerConfiguredControllers(array $config)
+ * @method static void registerFromFile(string $routes)
+ */
+class LambdaRoute extends Facade
+{
+    protected static function getFacadeAccessor(): string
+    {
+        return 'lbb.router';
+    }
+}
