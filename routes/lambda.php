1049283506e0fde24c79a7e4f446fcf3f8736567
Bubba <rob@stechstudio.com>
2019-10-15 11:00:43 -0400

    Register the routes

--- routes/lambda.php
+++ routes/lambda.php
@@ -1,10 +1,23 @@
 <?php declare(strict_types=1);
 
-use STS\AwsEvents\Events;
+use STS\AwsEvents\Events\ApiGatewayProxyRequest;
+use STS\AwsEvents\Events\ApplicationLoadBalancer;
+use STS\AwsEvents\Events\Sqs;
 use STS\LBB\Facades\LambdaRoute;
-use STS\LBB\Lambda\Controllers;
+use STS\LBB\Lambda\Controllers\VaporAPIGateway;
+use STS\LBB\Lambda\Controllers\VaporElb;
+use STS\LBB\Lambda\Controllers\VaporWorker;
 
 LambdaRoute::register(
-    Events\Sqs::class,
-    Controllers\VaporWorker::class);
+    Sqs::class,
+    VaporWorker::class);
 
+LambdaRoute::register(
+    ApplicationLoadBalancer::class,
+    VaporElb::class
+);
+
+LambdaRoute::register(
+    ApiGatewayProxyRequest::class,
+    VaporAPIGateway::class
+);
